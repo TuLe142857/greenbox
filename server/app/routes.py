1f16a4c5fc964edb3779ee4494c9224770502448
tule <tuleaibk@gmail.com>
2026-01-02 19:10:47 +0700

    update

--- server/app/routes.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask, Blueprint, render_template
-
-app_routes = Blueprint('app_routes', __name__)
-
-@app_routes.route("/")
-def index():
-    return render_template("index.html", msg="Hello")
